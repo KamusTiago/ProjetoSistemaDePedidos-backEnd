039b7e50a9f41a8e2c2a676e84a681a0f2c6c02d
KamusTiago <tiagokamus40@gmail.com>
2021-01-26 12:42:05 -0300

    Criando operacao de instanciacao

--- src/main/java/com/kamustiago/kamus/KamusApplication.java
+++ src/main/java/com/kamustiago/kamus/KamusApplication.java
@@ -1,13 +1,33 @@
 package com.kamustiago.kamus;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class KamusApplication {
+import com.kamustiago.kamus.domain.Categoria;
+import com.kamustiago.kamus.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class KamusApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(KamusApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
