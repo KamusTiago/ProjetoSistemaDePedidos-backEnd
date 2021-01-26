daf5f2598d709c200575466fd04832ec9f82d47b
KamusTiago <tiagokamus40@gmail.com>
2021-01-26 14:55:37 -0300

    Ajustes no endpoint/ categorias/{id}

--- src/main/java/com/kamustiago/kamus/services/CategoriaService.java
+++ src/main/java/com/kamustiago/kamus/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.kamustiago.kamus.domain.Categoria;
 import com.kamustiago.kamus.repositories.CategoriaRepository;
+import com.kamustiago.kamus.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,8 +17,10 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		 Optional<Categoria> obj = repo.findById(id);
+		 return obj.orElseThrow(() -> new ObjectNotFoundException(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+				} 
+
 	}
-	
-}
+
