fd9d5b3e2a29dcbbc774e2d05463f57d55f79537
KamusTiago <tiagokamus40@gmail.com>
2021-01-26 09:45:58 -0300

    Projeto criado

---
+++ src/main/java/com/kamustiago/kamus/KamusApplication.java
@@ -0,0 +1,13 @@
+package com.kamustiago.kamus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamusApplication.class, args);
+	}
+
+}
